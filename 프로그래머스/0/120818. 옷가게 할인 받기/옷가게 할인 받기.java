1e71a84f370d289f195f99089a110763a962fa3e
윤종일 <47919911+pie0902@users.noreply.github.com>
2024-01-22 02:38:29 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int price) {
+        double discountRate = 0;
+
+        if (price >= 500000) { // 
+            discountRate = 0.2;
+        } else if (price >= 300000) { 
+            discountRate = 0.1;
+        } else if (price >= 100000) { 
+            discountRate = 0.05;
+        }
+
+        int discountedPrice = (int)(price * (1 - discountRate));
+        return discountedPrice;
+    }
+}
