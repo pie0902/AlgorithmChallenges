22b89436cbef5f02f9c21c62a0fae59930731a96
윤종일 <47919911+pie0902@users.noreply.github.com>
2024-01-26 11:27:59 +0900

    [level 1] Title: 시저 암호, Time: 53.99 ms, Memory: 107 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String s, int n) {
+        String answer = "";
+        for (int i = 0; i < s.length(); i++) {
+            char a = s.charAt(i);
+
+            if (a != ' ') {
+                if (a >= 'A' && a <= 'Z') {
+                    a = (char) ((a - 'A' + n) % 26 + 'A');
+                } else if (a >= 'a' && a <= 'z') {
+                    a = (char) ((a - 'a' + n) % 26 + 'a');
+                }
+            }
+
+            answer += a;
+        }
+        return answer;
+    }
+}
