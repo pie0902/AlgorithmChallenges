575de1e0fc9fd1e912b2fe3c9e1ef50573016259
윤종일 <47919911+pie0902@users.noreply.github.com>
2024-03-06 17:48:52 +0900

    [level 0] Title: 편지, Time: 0.15 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        System.out.println(message.length());
+        answer = message.length() * 2;
+        return answer;
+    }
+}
