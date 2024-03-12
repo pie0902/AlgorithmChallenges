8cbffb03c5be7fa4c25df30babf3b3396543b791
윤종일 <47919911+pie0902@users.noreply.github.com>
2024-03-12 16:19:36 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.03 ms, Memory: 66 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE;
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+
+        for (int num : numbers) {
+            if (num > max1) {
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2) {
+                max2 = num;
+            }
+            if (num < min1) {
+                min2 = min1;
+                min1 = num;
+            } else if (num < min2) {
+                min2 = num;
+            }
+        }
+        return Math.max(max1 * max2, min1 * min2);
+    }
+}
