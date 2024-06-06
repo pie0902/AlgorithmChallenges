5f74bd44638713ea5df6919408c9a372e60da0c3
윤종일 <47919911+pie0902@users.noreply.github.com>
2024-06-06 15:39:21 +0900

    [level 0] Title: 할 일 목록, Time: 0.05 ms, Memory: 80 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,15 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        List<String> list = new ArrayList<>();
+        for(int i = 0; i<todo_list.length;i++){
+            if(finished[i]==false){
+                list.add(todo_list[i]);   
+            }
+        }
+        String[] answer = list.toArray(new String[0]);
+        return answer;
+    }
+}
