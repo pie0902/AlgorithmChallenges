b82a068fc97704e735ce9de8c5278b2f11d61f8d
윤종일 <47919911+pie0902@users.noreply.github.com>
2024-08-12 16:41:20 +0900

    [level 1] Title: 폰켓몬, Time: 34.24 ms, Memory: 99 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,13 +2,21 @@ import java.util.*;
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        Set<Integer> set = new HashSet<>();
-        for(Integer num:nums){
-            set.add(num);
-        }  
-        
-        int cnt = nums.length/2;
-        answer = Math.min(set.size(),cnt);
+        Arrays.sort(nums);
+        List<Integer> lists = new ArrayList<>();
+        lists.add(nums[0]);
+        for(int i = 1; i<nums.length; i++){
+            if(nums[i-1]!=nums[i]){
+                lists.add(nums[i]);   
+            }
+        }
+        for(int i :lists){
+            System.out.println(i);
+        }
+        answer = lists.size();
+        if(answer>(nums.length/2)){
+            answer = nums.length/2;
+        }
         return answer;
     }
 }
