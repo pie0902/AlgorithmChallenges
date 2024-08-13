9da2a549c489451f76071674221a52bcf039b8db
윤종일 <47919911+pie0902@users.noreply.github.com>
2024-08-13 12:53:55 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.58 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] arr, int idx) {
+        int answer = 0;
+        for(int i = 0; i<arr.length; i++){
+            if(i>=idx){
+                if(arr[i]==1){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+}
