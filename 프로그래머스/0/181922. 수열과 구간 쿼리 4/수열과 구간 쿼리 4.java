723b0d73ee9b9583501edf7ca09906fed96c00e3
윤종일 <47919911+pie0902@users.noreply.github.com>
2024-08-21 18:46:35 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 4.24 ms, Memory: 91.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] q) {
+        for (int i = 0; i < q.length; i++) {
+            for (int j = q[i][0]; j <= q[i][1]; j++) {
+                if (j % q[i][2] == 0) { 
+                    arr[j] += 1;    
+                }
+            }
+        }
+        return arr;
+    }
+}
