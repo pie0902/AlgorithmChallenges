4b177bf61b60362c13b52c9f12619c02daae732a
윤종일 <47919911+pie0902@users.noreply.github.com>
2024-09-15 14:39:33 +0900

    [level 2] Title: 구명보트, Time: 11.36 ms, Memory: 69.7 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,22 +1,17 @@
 import java.util.*;
-
 class Solution {
-    public int solution(int[] people, int limit) {
-        Arrays.sort(people);  // 몸무게를 오름차순으로 정렬
-        int light = 0;  // 가벼운 사람의 인덱스
-        int heavy = people.length - 1;  // 무거운 사람의 인덱스
-        int answer = 0;  // 필요한 보트 개수
-        
-        while (light <= heavy) {
-            // 가장 가벼운 사람과 가장 무거운 사람을 함께 태울 수 있으면
-            if (people[light] + people[heavy] <= limit) {
-                light++;  // 가벼운 사람도 태움
+    public int solution(int[] p, int limit) {
+        int answer = 0;
+        int nomal = 0;
+        int heavy = p.length-1;
+        Arrays.sort(p);
+        while(nomal<=heavy){
+            if(p[nomal]+p[heavy]<=limit){
+                nomal++;
             }
-            // 무거운 사람은 보트에 태움
             heavy--;
-            answer++;  // 보트 하나 추가
+            answer++;
         }
-        
         return answer;
     }
 }
