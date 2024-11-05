ff39bea24ba8504720fc3020d8db02abcaa1e560
윤종일 <47919911+pie0902@users.noreply.github.com>
2024-11-05 15:55:51 +0900

    [level 2] Title: 미로 탈출, Time: 7.61 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,77 +1,61 @@
 import java.util.*;
-class Solution{
-    static class Point {
-        int row,col,d;
-        public Point(int row,int col, int d){
-            this.row = row;
-            this.col = col;
-            this.d = d;
-        }
+class Solution {
+    public static boolean check(int nRow,int nCol,boolean[][] v, char[][] c){
+        return nRow >= 0 && nCol >= 0 && nRow < c.length && nCol < c[0].length && c[nRow][nCol] != 'X' && !v[nRow][nCol];
     }
-    public static boolean isValid(int row, int col, char[][] maze, boolean[][] visited){
-        return row>=0&&col>=0&&row<maze.length&&col<maze[0].length&&maze[row][col]!='X'&& !visited[row][col];
-    }
-    public static int bfs(char[][] maze,int startRow, int startCol, char target){
+    
+    public static int[] bfs(int sRow,int sCol,char[][] c,char target){
         int[] rowMove = {1,-1,0,0};
         int[] colMove = {0,0,-1,1};
-        boolean[][] visited = new boolean[maze.length][maze[0].length];
-        Queue<Point> queue = new LinkedList<>();
-        queue.add(new Point(startRow,startCol,0));
-        visited[startRow][startCol] = true;
-        while(!queue.isEmpty()){
-            Point now = queue.poll();
-            if(maze[now.row][now.col]==target){
-                return now.d;
+        boolean[][] v = new boolean[c.length][c[0].length];
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{sRow,sCol,0});
+        v[sRow][sCol] = true;
+        while(!q.isEmpty()){
+            int[] now = q.poll();
+            int row = now[0];
+            int col = now[1];
+            int cnt = now[2];
+            if(c[row][col]==target){
+                return new int[]{row,col,cnt};
             }
             for(int i = 0; i<4; i++){
-                int newRow = now.row + rowMove[i];
-                int newCol = now.col + colMove[i];
-                if(isValid(newRow,newCol,maze,visited)){
-                    visited[newRow][newCol] = true;
-                    queue.add(new Point(newRow,newCol,now.d+1));   
+                int newRow = row + rowMove[i];
+                int newCol = col + colMove[i];
+                if(check(newRow,newCol,v,c)){
+                    v[newRow][newCol] = true;
+                    int[] newPoint = {newRow,newCol,cnt+1};
+                    q.offer(newPoint);
                 }
             }
         }
-        return -1;
+        return new int[]{-1,-1,-1};
     }
     
     
-    
-    
-    
-    
-    
-    
-    public int solution(String[] maps) {
-        int startRow = 0, startCol = 0;
-        int leverRow = 0, leverCol = 0;
-        int exitRow = 0, exitCol = 0;
-        char[][] maze = new char[maps.length][maps[0].length()];
-        for(int i = 0; i<maps.length; i++){
-            for(int j = 0; j<maps[0].length(); j++){
-                maze[i][j] = maps[i].charAt(j);
-                if(maze[i][j]=='S'){
-                    startRow = i;
-                    startCol = j;
-                }
-                else if(maze[i][j] == 'L'){
-                    leverRow = i;
-                    leverCol = j;
-                }
-                else if(maze[i][j] == 'E'){
-                    exitRow = i;
-                    exitCol = j;
-                }
+    public int solution(String[] m) {
+        int answer = 0;
+        char[][] c = new char[m.length][m[0].length()];
+        boolean[][] v = new boolean[m.length][m[0].length()];
+        int row=0,col=0;
+        for(int i = 0; i<m.length; i++){
+            for(int j = 0; j<m[0].length(); j++){
+                c[i][j] = m[i].charAt(j);
+                if(c[i][j] == 'S'){
+                    row = i;
+                    col = j;
+                } 
             }
         }
-        // 시작 지점에서 레버까지 최단 거리
-        int toLever = bfs(maze, startRow, startCol, 'L');
-        if (toLever == -1) return -1;
-
-        // 레버에서 출구까지 최단 거리
-        int toExit = bfs(maze, leverRow, leverCol, 'E');
-        if (toExit == -1) return -1;
-
-        return toLever + toExit;
+        int[] lever = bfs(row,col,c,'L');
+        if(lever[0] == -1){
+            return -1;
+        }
+        int[] exit = bfs(lever[0],lever[1],c,'E');
+        if(exit[0] == -1){
+            return -1;
+        }
+        answer = lever[2]+exit[2];
+        return answer;
     }
 }
